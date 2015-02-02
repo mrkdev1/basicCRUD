41009533de9bb56d699efea49a316f36927fe639
MARK_ROVETTA <mkrnrt@gmail.com>
2015-02-02 14:00:53 -0800

    simple examples of PATCH from perl

---
+++ perl/Perl_patch/index.pl
@@ -0,0 +1,24 @@
+#The following script gets global-settings information about the BigIP 
+#from the iControl REST API
+
+use strict;
+use warnings;
+use LWP::UserAgent;
+use open qw(:std :utf8);
+my $ua = LWP::UserAgent->new( ssl_opts => { verify_hostname => 0 } );
+
+$ua->credentials(
+    '<management_ip>:443',
+    'Enterprise Manager',
+    'admin' => 'admin'
+  );
+
+my $response = $ua->get('https://<management_ip>/mgmt/tm/sys/global-settings');
+if ( $response->is_success ) {
+    open(my $fh, '>', 'doodle.json');
+    print $fh $response->decoded_content;
+    close $fh;
+}
+else {
+    die $response->status_line;
+}
---
+++ perl/Perl_patch/indexQ.pl
@@ -0,0 +1,40 @@
+#Patches global-settings info of test.data into BIGIP
+use strict;
+use warnings;
+use LWP::UserAgent;
+use open qw(:std :utf8);
+ 
+my $ua = LWP::UserAgent->new( ssl_opts => { verify_hostname => 0 } );
+
+$ua->credentials(
+    '<management_ip>:443',
+    'Enterprise Manager',
+    'admin' => 'admin'
+  );
+ 
+my $server_endpoint = "https://<management_ip>:443/mgmt/tm/sys/global-settings";
+ 
+# set custom HTTP request header fields
+my $req = HTTP::Request->new(PATCH => $server_endpoint);
+$req->header('content-type' => 'application/json');
+ 
+# add PATCH data to HTTP request body
+open INPUT, "<test3.json";
+undef $/;
+my $post_data = <INPUT>;
+close INPUT;
+$/ = "\n";     #Restore for normal behaviour later in script
+
+$req->content($post_data);
+ 
+my $resp = $ua->request($req);
+if ($resp->is_success) {
+    open(my $fh, '>', 'doodle.json');
+    print $fh $resp->decoded_content;
+    close $fh;
+}
+else {
+    print "HTTP POST error code: ", $resp->code, "\n";
+    print "HTTP POST error message: ", $resp->message, "\n";
+}
+ 
